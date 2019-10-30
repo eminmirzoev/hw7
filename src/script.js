9efbe2371f54fbe321c52aa835ef501ac45e7cf6
Emin Mirzoev <m.emin123@live.com>
2019-10-30 18:32:49 +0400

    1

---
+++ src/script.js
@@ -0,0 +1,29 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operators = '';
+let isSecond = false;
+const input = document.getElementById('input');
+
+const calculator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const showValue = value => {
+    inputValue = parseInt(value);
+};
