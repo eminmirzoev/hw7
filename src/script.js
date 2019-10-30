435ff994213f09ebe9741a8caa2c64f04d7d07bd
Emin Mirzoev <m.emin123@live.com>
2019-10-30 20:37:45 +0400

    1

--- src/script.js
+++ src/script.js
@@ -1,29 +1,63 @@
 let firstNumber = 0;
 let secondNumber = 0;
-let operators = '';
+let operator = '';
 let isSecond = false;
 const input = document.getElementById('input');
 
-const calculator
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const calculator = () => {
+    switch(operator) {
+        case '+':
+            return firstNumber + secondNumber;
+        case '-':
+            return firstNumber - secondNumber;
+        case '*':
+            return firstNumber * secondNumber;
+        case '/':
+            return !secondNumber ? 0: firstNumber / secondNumber;
+        default:
+            return 0;
+    }
+}
 
 const showValue = value => {
-    inputValue = parseInt(value);
+    input.value = value;
+}
+
+const clearAll = () => {
+    showValue(0);
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = 0;
+}
+
+const clickDigit = value => {
+    if (isSecond) {
+    showValue(0);
+    isSecond = false;
+    }
+}
+    
+let result = input.value + value;
+let number = parseInt(result);
+showValue(number);
+
+if (!operator) {
+    firstNumber = number;
+} else {
+    secondNumber = number;
 };
+    
+const operator = op => {
+    if (op === '=') {
+const result = calculate();
+
+operator = "";
+showValue(result);
+} else {
+    operator = op;
+    isSecond = true;
+}
+    
+const showValue = value => {
+    inputValue = parseInt(value);
+}}
